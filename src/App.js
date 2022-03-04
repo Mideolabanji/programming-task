8880fd8953ef9056df7071da743e11f639cbac37
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-05 00:57:16 +0100

    included heading (links) and styled the page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="containe">
-        <h1>Hello</h1>
+      <div className="app-body">
+        <h1>Complete your Purchase</h1>
+        <div className="headings">
+          <a href="#">
+            <h3>Personal Info</h3>
+          </a>
+          <a href="#">
+            <h3>Billing Info</h3>
+          </a>
+          <a href="#">
+            <h3>Confirm Payment</h3>
+          </a>
+          <hr />
+        </div>
       </div>
     </div>
   );

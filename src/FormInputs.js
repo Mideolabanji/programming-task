06264e342cec12f939287c8411c3c810938f1119
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-05 22:44:23 +0100

    styled the state select element

--- src/FormInputs.js
+++ src/FormInputs.js
@@ -68,36 +68,34 @@ export default function FormInputs() {
             />
           </div>
           <div className="col-5">
-            <label
-              for="exampleFormControlTextarea1"
-              className="form-label mb-3"
-            >
+            <label for="inputState" className="form-label mb-3">
               State
             </label>
-            <input
-              type="text"
-              className="form-control form-control-lg"
-              id="exampleFormControlTextarea1"
-              placeholder="Lagos"
-            />
+            <select
+              id="inputState"
+              className="form-select form-select-lg select-input"
+            >
+              <option selected>Lagos</option>
+              <option>...</option>
+            </select>
           </div>
         </div>
+        <footer>
+          <a href="#">
+            <button type="submit" className="btn btn-lg mt-5 ms-2 next-btn">
+              Next
+            </button>
+          </a>
+          <a href="#">
+            <button
+              type="button"
+              className="btn btn-link cancel-payment-link btn-lg mt-5"
+            >
+              Cancel Payment
+            </button>
+          </a>
+        </footer>
       </form>
-      <footer>
-        <a href="#">
-          <button type="submit" className="btn btn-lg mt-5 ms-2 next-btn">
-            Next
-          </button>
-        </a>
-        <a href="#">
-          <button
-            type="button"
-            className="btn btn-link cancel-payment-link btn-lg mt-5"
-          >
-            Cancel Payment
-          </button>
-        </a>
-      </footer>
     </div>
   );
 }

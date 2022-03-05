8d09b45186d99e20902202b802e7e8c6f1bcb19f
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-05 21:19:42 +0100

    styled and included all the input forms for the page

--- src/FormInputs.js
+++ src/FormInputs.js
@@ -3,27 +3,87 @@ import React from "react";
 export default function FormInputs() {
   return (
     <div>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">
-          Email address
+      <div className="mb-5 mt-5">
+        <label for="exampleFormControlTextarea1" className="form-label mb-3">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          id="exampleFormControlTextarea1"
+          placeholder="Opara Linus Ahmed"
+        />
+      </div>
+      <div className="mb-5">
+        <label for="exampleFormControlInput1" className="form-label mb-3">
+          Email address <span className="required"> *</span>
         </label>
         <input
           type="email"
-          class="form-control form-control-lg"
+          className="form-control form-control-lg"
           id="exampleFormControlInput1"
-          placeholder="name@example.com"
+          placeholder="OparaLinusAhmed@devmail.com"
         />
       </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          Example textarea
+      <div className="mb-5">
+        <label for="exampleFormControlTextarea1" className="form-label mb-3">
+          Address 1
         </label>
-        <textarea
-          class="form-control"
+        <input
+          type="text"
+          className="form-control form-control-lg"
           id="exampleFormControlTextarea1"
-          rows="3"
-        ></textarea>
+          placeholder="The address of the user goes here"
+        />
       </div>
+      <div className="mb-5">
+        <label for="exampleFormControlTextarea1" className="form-label mb-3">
+          Address 2
+        </label>
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          id="exampleFormControlTextarea1"
+          placeholder="and here"
+        />
+      </div>
+      <div className="row">
+        <div className="col-7">
+          <label for="exampleFormControlTextarea1" className="form-label mb-3">
+            Local Government
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            id="exampleFormControlTextarea1"
+            placeholder="Surulere"
+          />
+        </div>
+        <div className="col-5">
+          <label for="exampleFormControlTextarea1" className="form-label mb-3">
+            State
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            id="exampleFormControlTextarea1"
+            placeholder="Lagos"
+          />
+        </div>
+      </div>
+      <a className="#">
+        <button type="button" className="btn btn-lg mt-5 ms-2 next-btn">
+          Next
+        </button>
+      </a>
+      <a href="#">
+        <button
+          type="button"
+          className="btn btn-link cancel-payment-link btn-lg mt-5"
+        >
+          Cancel Payment
+        </button>
+      </a>
     </div>
   );
 }

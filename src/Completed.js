77304e67280bd320465acb0b568b2afa5a810498
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-07 23:03:06 +0100

    styled the background of the ✔ icon

--- src/Completed.js
+++ src/Completed.js
@@ -1,18 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Completed() {
   return (
     <div className="app-body">
       <div className="card completed-card">
-        <img
-          className="icon"
-          height="45"
-          width="45"
-          src="https://www.fontisto.com/assets/icons/svg/check.svg"
-        />
         <div className="card-body">
+          <div className="icon">
+            <FontAwesomeIcon icon={faCheck} color="#0AC880" size="3x" />
+          </div>
           <h1 className="card-title">Purchase Completed</h1>
-          <p className="card-text">
+          <p className="card-text text">
             Please check your email for details concerning this transaction
           </p>
           <a href="#" className="card-link">

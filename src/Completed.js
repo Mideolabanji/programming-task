af40de16a772d56659914022e3823cad54428ee7
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-07 21:48:45 +0100

    installed font awesome

--- src/Completed.js
+++ src/Completed.js
@@ -1,10 +1,15 @@
 import React from "react";
-import Heading from "./Heading";
 
 export default function Completed() {
   return (
     <div className="app-body">
       <div className="card completed-card">
+        <img
+          className="icon"
+          height="45"
+          width="45"
+          src="https://www.fontisto.com/assets/icons/svg/check.svg"
+        />
         <div className="card-body">
           <h1 className="card-title">Purchase Completed</h1>
           <p className="card-text">

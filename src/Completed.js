bce6c6e90c8fe1ec231f3e9044aefd62512df0e8
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-07 20:17:26 +0100

    included complete card content

--- src/Completed.js
+++ src/Completed.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Heading from "./Heading";
 
 export default function Completed() {
-  return "Complete page";
+  return (
+    <div className="app-body">
+      <div className="card completed-card">
+        <div className="card-body">
+          <h1 className="card-title">Purchase Completed</h1>
+          <p className="card-text">
+            Please check your email for details concerning this transaction
+          </p>
+          <a href="#" className="card-link">
+            Return Home
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }

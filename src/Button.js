a9df9f08369e7b5c57387f76163d0200802f833d
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-08 21:34:31 +0100

    updated button.js

--- src/Button.js
+++ src/Button.js
@@ -4,11 +4,7 @@ import { Link } from "react-router-dom";
 export default function Button() {
   return (
     <Link to="/next">
-      <button
-        onClick={billingInfo}
-        type="submit"
-        className="btn btn-lg mt-5 ms-2 next-btn"
-      >
+      <button type="submit" className="btn btn-lg mt-5 ms-2 next-btn">
         Next
       </button>
     </Link>

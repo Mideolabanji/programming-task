b10fa754e9522c64f350548bfbefea8e302f377b
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-08 20:41:40 +0100

    included state to the component

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Button() {
   return (
     <Link to="/next">
-      <button type="submit" className="btn btn-lg mt-5 ms-2 next-btn">
+      <button
+        onClick={billingInfo}
+        type="submit"
+        className="btn btn-lg mt-5 ms-2 next-btn"
+      >
         Next
       </button>
     </Link>
